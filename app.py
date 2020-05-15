acb55cd889e04f5330786555d07ca95be981e077
johnblanco <j.pabloblanco@gmail.com>
2020-05-15 14:41:13 -0300

    con tipos

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output
+from plotly.graph_objs import Layout, Bar
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
+
+#TODO procesar los datos para que no haya departamento / tipo de c/mes sin un valor, llenar con 0
+#TODO procesar los datos para que no haya departamento / tipo de c/mes sin un valor, llenar con 0
+#TODO procesar los datos para que no haya departamento / tipo de c/mes sin un valor, llenar con 0
+#TODO procesar los datos para que no haya departamento / tipo de c/mes sin un valor, llenar con 0
 
 app.layout = html.Div(children=[
     html.H1(children='Empresa en el Dia'),
@@ -18,9 +24,10 @@ app.layout = html.Div(children=[
         Cantidad de empresas creadas, entre 2017 - 2019, a través del sistema de Empresa en el día, agrupado por departamento (Salto, Montevideo y Maldonado).
     '''),
 
-    html.Label("La gráfica muestra los totales de cada mes (unipersonales, SRL, SA, sociedades de hecho, monotributo MIDES)"),
+    html.Div(['El dataset es el provisto por Agesic en ', html.A('catalogodatos.gub.uy',
+                                                                 href='https://catalogodatos.gub.uy/dataset/agesic-creacion-de-empresas-a-traves-de-empresa-en-el-dia')]),
 
-    html.Div(['El dataset es el provisto por Agesic en ', html.A('catalogodatos.gub.uy', href='https://catalogodatos.gub.uy/dataset/agesic-creacion-de-empresas-a-traves-de-empresa-en-el-dia')]),
+    html.H2('Totales por mes'),
 
     dcc.RadioItems(
         id='year',
@@ -37,6 +44,47 @@ app.layout = html.Div(children=[
         id='graph'
     ),
 
+    html.H2('Agrupado por tipo'),
+
+    dcc.RadioItems(
+        id='year2',
+        options=[
+            {'label': '2017', 'value': '2017'},
+            {'label': '2018', 'value': '2018'},
+            {'label': '2019', 'value': '2019'}
+        ],
+        value='2017',
+        labelStyle={'display': 'inline-block'}
+    ),
+
+    dcc.RadioItems(
+        id='city2',
+        options=[
+            {'label': 'Montevideo', 'value': 'Montevideo'},
+            {'label': 'Salto', 'value': 'Salto'},
+            {'label': 'Maldonado', 'value': 'Maldonado'}
+        ],
+        value='Montevideo',
+        labelStyle={'display': 'inline-block'}
+    ),
+
+    dcc.Dropdown(
+        id='company_type',
+        options=[
+            {'label': 'SA', 'value': 'SA'},
+            {'label': 'SRL', 'value': 'SRL'},
+            {'label': 'Unipersonal', 'value': 'Unipersonal'},
+            {'label': 'Sociedad de hecho', 'value': 'Sociedad de hecho'},
+            {'label': 'Monotributo MIDES', 'value': 'Monotributo MIDES'}
+        ],
+        value=['SA', 'SRL', 'Monotributo MIDES', 'Unipersonal', 'Sociedad de hecho'],
+        multi=True
+    ),
+
+    dcc.Graph(
+        id='graph2'
+    ),
+
     html.A('Link a Github', href='https://github.com/johnblanco/empresa_en_el_dia'),
     html.Br(),
     html.A('Tareas pendientes', href='https://github.com/johnblanco/empresa_en_el_dia/issues'),
@@ -45,11 +93,50 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
+    Output('graph2', 'figure'),
+    [Input('year2', 'value'),
+     Input('city2', 'value'),
+     Input('company_type', 'value'),
+     ]
+)
+def update_inputs_graph2(year2, city2, company_type):
+    df = pd.read_csv('data.csv').sort_values(
+        by='date')
+    df.rename(columns={"srl_count": "SRL", "sa_count": "SA",
+                       'mono_mides_count': 'Monotributo MIDES',
+                       'sociedad_de_hecho_count': 'Sociedad de hecho',
+                       'unipersonal_count': 'Unipersonal'
+                       }, inplace=True)
+    df = df[(df.year == int(year2)) & (df.city == city2)]
+
+    title = '{}-{}'.format(year2, city2)
+
+    return {
+        'data': bars_from_company_type(df, company_type),
+        'layout': Layout(barmode='stack', title=title)
+    }
+
+
+def bars_from_company_type(df, company_type):
+    bars = []
+    all_types = ['SA', 'SRL', 'Monotributo MIDES', 'Unipersonal', 'Sociedad de hecho']
+    for c_type in all_types:
+        if c_type in company_type:
+            bars.append(Bar(
+                x=df.month,
+                y=df[c_type],
+                name=c_type
+            ))
+
+    return bars
+
+
+@app.callback(
     Output('graph', 'figure'),
     [Input('year', 'value')]
 )
 def update_year(year):
-    df = pd.read_csv('https://raw.githubusercontent.com/johnblanco/empresa_en_el_dia/master/data.csv').sort_values(
+    df = pd.read_csv('data.csv').sort_values(
         by='date')
     df_mvd = df[(df.year == int(year)) & (df.city == 'Montevideo')]
     df_salto = df[(df.year == int(year)) & (df.city == 'Salto')]
@@ -71,4 +158,4 @@ def update_year(year):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
--- parse_data.py
+++ parse_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 month_list = 'enero|febrero|marzo|abril|mayo|junio|julio|agosto|setiembre|octubre|noviembre|diciembre'.split("|")
 
+
 def extract_city(f):
     if 'montevideo' in f or 'mdeo' in f:
         return 'Montevideo'
@@ -18,6 +19,61 @@ def extract_city(f):
 def month_from_name(name):
     month = month_list.index(name) + 1
     return f'{month:02}'
+
+
+def missing_months_df():
+    missing = ['2017-01,Maldonado',
+               '2017-02,Maldonado',
+               '2017-03,Maldonado',
+               '2017-04,Maldonado',
+               '2017-05,Maldonado',
+               '2017-06,Maldonado',
+               '2017-07,Maldonado',
+               '2017-08,Maldonado',
+               '2017-09,Maldonado',
+               '2017-10,Maldonado',
+               '2017-11,Maldonado',
+               '2017-12,Maldonado',
+               '2018-01,Maldonado',
+               '2018-02,Maldonado',
+               '2018-03,Maldonado',
+               '2018-04,Maldonado',
+               '2018-05,Maldonado',
+               '2019-05,Montevideo',
+               '2019-07,Montevideo',
+               '2019-08,Montevideo',
+               '2019-09,Montevideo',
+               '2019-10,Montevideo',
+               '2019-05,Maldonado',
+               '2019-07,Maldonado',
+               '2019-08,Maldonado',
+               '2019-09,Maldonado',
+               '2019-10,Maldonado',
+               '2019-11,Maldonado',
+               '2019-12,Maldonado',
+               '2019-05,Salto',
+               '2019-07,Salto',
+               '2019-08,Salto',
+               '2019-09,Salto',
+               '2019-10,Salto',
+               '2019-11,Salto',
+               '2019-12,Salto']
+    rows = {}
+    for a in missing:
+        date = a.split(',')[0]
+        city = a.split(',')[1]
+        rows[a] = [
+            'not_found',
+            city,
+            date,
+            date[0:4],
+            month_list[int(date[5:7]) - 1],
+            0, 0, 0, 0, 0, 0]
+
+    return pd.DataFrame.from_dict(rows, orient='index',
+                                  columns=['file', 'city', 'date', 'year', 'month', 'srl_count', 'sa_count',
+                                           'mono_mides_count', 'unipersonal_count', 'sociedad_de_hecho_count',
+                                           'total'])
 
 
 def extract_date(filename):
@@ -36,17 +92,49 @@ def extract_date(filename):
     return '-'
 
 
+def load_file_details(filename):
+    df = pd.read_csv(os.getcwd() + '/csvs/' + filename, encoding='latin1')
+    if '-2019.csv' in filename:
+        df = pd.read_csv(os.getcwd() + '/csvs/' + filename, sep=';', encoding='latin1')
+
+    sa_count = 0
+    srl_count = 0
+    mono_mides_count = 0
+    unipersonal_count = 0
+    sociedad_de_hecho_count = 0
+
+    df = df.rename(columns={"Tipo ": "TIPO", "Tipo": "TIPO", "Nombre del proceso": "TIPO"})
+
+    col = "TIPO"
+
+    sa_count = len(df[(df[col].str.contains('SA')) | (df[col].str.contains('SOCIEDAD AN'))])
+    srl_count = len(df[(df[col].str.contains('SRL'))])
+    mono_mides_count = len(df[(df[col].str.contains('MONO')) | (df[col].str.contains('MS'))])
+    unipersonal_count = len(df[(df[col].str.contains('UNI'))])
+    sociedad_de_hecho_count = len(df[(df[col].str.contains('SH')) | (df[col].str.contains('SOCIEDAD DE HECHO'))])
+
+    return {
+        'sa_count': sa_count,
+        'srl_count': srl_count,
+        'mono_mides_count': mono_mides_count,
+        'unipersonal_count': unipersonal_count,
+        'sociedad_de_hecho_count': sociedad_de_hecho_count
+    }
+
+
 def files_summary(files):
     d = {}
     for filename in files:
         df = pd.read_csv(os.getcwd() + '/csvs/' + filename, encoding='latin1')
         total = len(df)
+        file_detail = load_file_details(filename)
         d[filename] = {
             'total': total,
-            'sa_count': -1,#TODO
-            'mono_mides_count': -1,#TODO
-            'unipersonal_count': -1,#TODO
-            'sociedad_de_hecho_count': -1#TODO
+            'sa_count': file_detail['sa_count'],
+            'srl_count': file_detail['srl_count'],
+            'mono_mides_count': file_detail['mono_mides_count'],
+            'unipersonal_count': file_detail['unipersonal_count'],
+            'sociedad_de_hecho_count': file_detail['sociedad_de_hecho_count']
         }
 
     return d
@@ -58,16 +146,16 @@ def clean_data():
     df['city'] = df.file.apply(lambda f: extract_city(f))
     df['date'] = df.file.apply(lambda f: extract_date(f))
     df['year'] = df.date.apply(lambda d: d[0:4])
-    df['month'] = df.date.apply(lambda d: month_list[int(d[5:7])-1])
+    df['month'] = df.date.apply(lambda d: month_list[int(d[5:7]) - 1])
 
     file_summary = files_summary(files)
 
-    df['srl_count'] = df.file
-    new_columns = ['sa_count', 'mono_mides_count', 'unipersonal_count', 'sociedad_de_hecho_count', 'total']
+    new_columns = ['srl_count', 'sa_count', 'mono_mides_count', 'unipersonal_count', 'sociedad_de_hecho_count', 'total']
     for col in new_columns:
         df[col] = df.file.apply(lambda f: file_summary[f][col])
 
-    return df
+    df2 = missing_months_df()
+    return pd.concat([df, df2], sort=False)
 
 
 if __name__ == '__main__':
